dbe3fe29e8947400ec2f56a81756cd90200cb1e2
Eslam Esmael <EslamEsmael012@Gmail.com>
2022-03-25 15:18:43 +0200

    Adding LoginRegisterActivity, LoginFragment,SignUpFragment , setting ui

---
+++ app/src/main/java/com/example/minerva/usersystem/LoginRegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.example.minerva.usersystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import com.example.minerva.R
+import com.example.minerva.databinding.ActivityLoginRegisterBinding
+
+class LoginRegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+
+    }
+}
