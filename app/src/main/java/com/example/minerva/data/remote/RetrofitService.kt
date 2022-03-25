017383677da88e0086d0ee7665e03c3d7328c9b2
Walid Zain <walidzeno@gmail.com>
2022-03-25 23:10:35 +0200

    add adapters for news item

--- app/src/main/java/com/example/minerva/data/remote/RetrofitService.kt
+++ app/src/main/java/com/example/minerva/data/remote/RetrofitService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 const val API_KEY = "cfb6154624504c3a9e93bc4ab969e941"
 
 interface RetrofitService {
-    @GET("top-headlines?")
+    @GET("top-headlines")
     suspend fun getAllStatusWeatherByLatLon(
         @Query("country") country: String = "us",
         @Query("apiKey") apiKey: String = API_KEY
