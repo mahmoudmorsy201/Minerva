fc3df93fb700745391df557c30d0d0108cef74a0
Eslam Esmael <EslamEsmael012@Gmail.com>
2022-03-26 16:55:16 +0200

    Fix Main

--- app/src/main/java/com/example/minerva/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/minerva/ui/home/HomeViewModel.kt
@@ -1,23 +1,16 @@
 package com.example.minerva.ui.home
 
 
-import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.navigation.Navigation
 import com.example.minerva.R
 import com.example.minerva.data.model.Article
 import com.example.minerva.data.model.NewsDto
 import com.example.minerva.data.repository.RepositoryInterface
 import com.example.minerva.util.Constants.ARTICLE_KEY
-import com.example.minerva.util.InternetConnectivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import com.example.minerva.data.model.Article
-import com.example.minerva.data.model.NewsDto
-import com.example.minerva.data.repository.RepositoryInterface
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
