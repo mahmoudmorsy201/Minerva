e4e385dd2b9ac7b187cc7341d155e11e7d41477f
Eslam Esmael <EslamEsmael012@Gmail.com>
2022-03-26 04:50:47 +0200

    Search in home fragment using SearchView

--- app/src/main/java/com/example/minerva/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/minerva/ui/home/HomeFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.*
 import com.bumptech.glide.Glide
 import com.example.minerva.R
+import com.example.minerva.data.model.Article
 import com.example.minerva.data.model.NewsDto
 import com.example.minerva.databinding.FragmentHomeBinding
 import com.example.minerva.util.InternetConnectivity
@@ -23,6 +25,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private var _binding: FragmentHomeBinding? = null
     private lateinit var connectionLiveData: InternetConnectivity
 
+    private var mainList: MutableList<Article> = arrayListOf()
+    private var searchResultList: MutableList<Article> = arrayListOf()
+
     private val binding get() = _binding!!
     lateinit var newsAdapter: NewsAdapter
     override fun onCreateView(
@@ -35,12 +40,53 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         val root: View = binding.root
         connectionLiveData = InternetConnectivity(requireContext())
 
+        binding.searchHomeSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query!!.isNotEmpty()) {
+                    searchResultList.clear()
+                    searchResultList.addAll(
+                        mainList.filter {
+                            it.title!!.lowercase().contains(query.lowercase())
+                        }
+                    )
+                    newsAdapter.changeData(searchResultList)
+                } else {
+                    newsAdapter.changeData(mainList)
+                }
+                searchResultList.clear()
+
+                return true
+
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                if (query!!.isNotEmpty()) {
+                    searchResultList.clear()
+                    searchResultList.addAll(
+                        mainList.filter {
+                            it.title!!.lowercase().contains(query.lowercase())
+                        }
+                    )
+                    newsAdapter.changeData(searchResultList)
+                } else {
+                    newsAdapter.changeData(mainList)
+                }
+                searchResultList.clear()
+
+                return true
+            }
+
+
+        })
         newsAdapter = NewsAdapter(arrayListOf())
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.result
                 .flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
                 .distinctUntilChanged()
                 .collect { data ->
+                    mainList.clear()
+                    mainList.addAll(data.articles.subList(0, data.articles.size))
                     displayResult(data)
                 }
         }
@@ -53,6 +99,8 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                         .flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
                         .distinctUntilChanged()
                         .collect { data ->
+                            mainList.clear()
+                            mainList.addAll(data.articles.subList(0, data.articles.size))
                             displayResult(data)
                         }
                 }
